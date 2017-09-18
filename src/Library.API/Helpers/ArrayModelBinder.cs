35ba90de9f99d7a89affc58c73db4f5ae8d6f9c7
Drake Andreas IT/UTV-2-S <KONDRAN@scb.intra>
2017-09-18 19:17:21 +0200

    Add project files.

---
+++ src/Library.API/Helpers/ArrayModelBinder.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.API.Helpers
+{
+    public class ArrayModelBinder : IModelBinder
+    {
+        public Task BindModelAsync(ModelBindingContext bindingContext)
+        {
+            if (bindingContext == null)
+            {
+                throw new ArgumentNullException(nameof(bindingContext));
+            }
+
+            if (!bindingContext.ModelMetadata.IsEnumerableType)
+            {
+                bindingContext.Result = ModelBindingResult.Failed();
+                return Task.CompletedTask;
+            }
+
+            string value = bindingContext.ValueProvider.
+                GetValue(bindingContext.ModelName).ToString();
+
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                bindingContext.Result = ModelBindingResult.Success(null);
+                return Task.CompletedTask;
+            }
+
+            
+
+            Type elementType = bindingContext.ModelMetadata.ModelType.GenericTypeArguments[0];
+            var converter = TypeDescriptor.GetConverter(elementType);
+
+            var values = value.Split(new[] { "," }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => converter.ConvertFromString(x.Trim()))
+                .ToArray();
+
+            var typedValues = Array.CreateInstance(elementType, values.Length);
+            values.CopyTo(typedValues, 0);
+            bindingContext.Model = typedValues;
+
+            bindingContext.Result = ModelBindingResult.Success(bindingContext.Model);
+            return Task.CompletedTask;
+        }
+    }
+}
