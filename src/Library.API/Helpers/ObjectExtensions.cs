309d90f2fbdb51eb05a4717db7a059ae3206d9cc
Andreas Drake <andreas.drake@gmail.com>
2017-09-25 15:08:54 +0200

    Added extension for data shaping on single objects

---
+++ src/Library.API/Helpers/ObjectExtensions.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Reflection;
+
+namespace Library.API.Helpers
+{
+    public static class ObjectExtensions
+    {
+        public static ExpandoObject ShapeData<TSource>(this TSource source, string fields)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException("source");
+            }
+
+            var dataShapeObject = new ExpandoObject();
+            var propertyInfoList = new List<PropertyInfo>();
+
+            if (string.IsNullOrWhiteSpace(fields))
+            {
+                // all public properties should be in the ExpandoObject
+                var propertyInfos = typeof(TSource).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+                propertyInfoList.AddRange(propertyInfos);
+            }
+            else
+            {
+                string[] fieldsAfterSplit = fields.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+                foreach (string field in fieldsAfterSplit)
+                {
+                    string propertyName = field.Trim();
+
+                    var propertyInfo = typeof(TSource).GetProperty(propertyName, BindingFlags.IgnoreCase | BindingFlags.Public | BindingFlags.Instance);
+
+                    if (propertyInfo == null)
+                    {
+                        throw new Exception($"Property {propertyName} wasn't found on {typeof(TSource)}");
+                    }
+
+                    propertyInfoList.Add(propertyInfo);
+                }
+            }
+
+            foreach (PropertyInfo propertyInfo in propertyInfoList)
+            {
+                var propertyValue = propertyInfo.GetValue(source);
+
+                dataShapeObject.TryAdd(propertyInfo.Name, propertyValue);
+            }
+
+            return dataShapeObject;
+        }
+
+    }
+}
